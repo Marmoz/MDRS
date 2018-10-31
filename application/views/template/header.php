4c57aa7039168ca6e2b7128f62cce251bb466734
MarmoozNet <marmooznet@gmail.com>
2018-10-31 18:37:53 +0000

    Modif header

--- application/views/template/header.php
+++ application/views/template/header.php
@@ -14,7 +14,6 @@
  <link rel="stylesheet" type="text/css" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700|Roboto+Slab:400,700|Material+Icons" />
   <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css">
   <!-- CSS Files -->
-  <link href="<?php echo base_url('res/material-dashboard-html-v2.1.0') ?>/assets/css/material-dashboard.css" rel="stylesheet" />
   <!-- CSS Just for demo purpose, don't include it in your project -->
 
 
